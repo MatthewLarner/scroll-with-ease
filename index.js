b0b7e434c35a77027e8e1261b557951587c4df0c
iamalarner <matt.larner.dev@gmail.com>
2015-06-29 22:01:22 +1000

    cubic bezier easing

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var scrollToElement = require('scroll-element-into-view'),
-    Bezier = require('bezier-easing')
+    Bezier = require('bezier-easing');
 
 module.exports = function(target, time, curve) {
 
     curve = curve || 'ease';
 
-    var easingFunction = typeof curve === 'string' ? Bezier.css[curve]: Bezier.apply(null, curve)
+    var easingFunction = typeof curve === 'string' ? Bezier.css[curve]: Bezier.apply(null, curve);
 
     scrollToElement(target, time, easingFunction);
 };
